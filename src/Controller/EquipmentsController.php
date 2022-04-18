fa2e1074671a91f3a5d547d0a2402047041d8c1e
Jacques Lemoine <jacques.lemoine44@gmail.com>
2022-04-19 00:01:49 +0200

    v220418
    v220418

--- src/Controller/EquipmentsController.php
+++ src/Controller/EquipmentsController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Equipments;
 use App\Form\EquipmentsType;
 use App\Repository\EquipmentsRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,6 +13,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 /**
  * @Route("/equipments")
+ * @isGranted("ROLE_ADMIN")
  */
 class EquipmentsController extends AbstractController
 {
